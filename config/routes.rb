2c46ea5159a55ea436e5e4e7b6d72e172fb0ea8d
kyama/Pe <18105767+kyamadev@users.noreply.github.com>
2024-11-09 04:23:57 +0900

    Add dotenv-rails gem and configure ActionMailer for email delivery

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+  # ログインユーザー取得のルーティング
+  namespace :auth do
+    resources :sessions, only: %i[index]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
